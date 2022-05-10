61c28ba81f1293232e5ac1b60005b6cfb709dab0
Manon LE BIHAN <mle-biha@e2r5p14.42.fr>
2022-05-10 12:33:45 +0200

    ft_isdigit.c

---
+++ ft_isdigit.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/06 13:44:17 by mle-biha          #+#    #+#             */
+/*   Updated: 2022/05/10 11:44:10 by mle-biha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
+
+/*#include <stdio.h>
+#include <ctype.h>
+
+int main()
+{
+    char c;
+    c='5';
+    printf("Result when numeric character is passed (with ft): %d\n", ft_isdigit(c));
+    printf("Result when numeric character is passed (with og): %d\n", isdigit(c));
+
+    c='+';
+    printf("Result when non-numeric character is passed (with ft): %d\n", ft_isdigit(c));
+    printf("Result when non-numeric character is passed (with og): %d\n", isdigit(c));
+
+    c=' ';
+    printf("Result when nothing is passed (with ft): %d\n", ft_isdigit(c));
+    printf("Result when nothing is passed (with og): %d\n", isdigit(c));
+
+    return 0;
+}*/
