16a83d7389b43d24394b80280bce0ffa7e1dc60b
Manon LE BIHAN <mle-biha@e2r5p15.clusters.42paris.fr>
2022-05-11 12:39:18 +0200

    functional ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 15:24:20 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/05/10 15:36:16 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/05/11 12:36:37 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,11 +34,6 @@ int	ft_isdigit(char c)
 		return (0);
 }
 
-int	ft_ctoi(char c)
-{
-	return (c - '0');
-}
-
 int	ft_atoi(char *str)
 {
 	int	index;
@@ -49,22 +44,22 @@ int	ft_atoi(char *str)
 	while (str[index] != '\0' && ft_isspace(str[index]) != 0)
 		index++;
 	sign = 1;
-	while (str[index] != '\0' && ft_issign(str[index]) != 0)
+	if (ft_issign(str[index]) != 0)
 	{
 		if (str[index] == '-')
-			sign *= -1;
+			sign = -1;
 		index++;
 	}
 	value = 0;
 	while (str[index] != '\0' && ft_isdigit(str[index]) != 0)
 	{
-		value = value * 10 + ft_ctoi(str[index]);
+		value = value * 10 + str[index] - '0';
 		index++;
 	}
 	return (sign * value);
 }
 
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 
 int	main(int argc, char *argv[])
@@ -74,4 +69,4 @@ int	main(int argc, char *argv[])
 		printf("%d\n", ft_atoi(argv[1]));
 		printf("%d\n", atoi(argv[1]));
 	}
-}
+}*/
