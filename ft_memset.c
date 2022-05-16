2281accccef08f69b8d1d128b093ea7a4a1336fb
Manon Le Bihan <manonlebihan1999@gmail.com>
2022-05-16 21:00:00 +0200

    ft_memset

---
+++ ft_memset.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: manonlebihan <manonlebihan@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/16 20:51:43 by manonlebiha       #+#    #+#             */
+/*   Updated: 2022/05/16 20:52:11 by manonlebiha      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	unsigned char	*p;
+
+	p = b;
+	while (len--)
+		*p++ = (unsigned char)c;
+	return (b);
+}
