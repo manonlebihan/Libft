3509ce9d7627d25e7b71292c3da67273455d307a
Manon Le Bihan <manonlebihan1999@gmail.com>
2022-05-17 14:35:56 +0200

    changes in ft_memset

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: manonlebihan <manonlebihan@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/16 20:51:43 by manonlebiha       #+#    #+#             */
-/*   Updated: 2022/05/16 20:52:11 by manonlebiha      ###   ########.fr       */
+/*   Updated: 2022/05/17 14:34:11 by manonlebiha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_memset(void *b, int c, size_t len)
 {
 	unsigned char	*p;
 
+	if (b == NULL)
+		return (NULL);
 	p = b;
 	while (len--)
 		*p++ = (unsigned char)c;
