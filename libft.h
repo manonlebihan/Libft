5670e403eced8097ac3425af02d39c4488a294a0
Manon Le Bihan <manonlebihan1999@gmail.com>
2022-05-20 17:54:38 +0200

    ft_memcmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: manonlebihan <manonlebihan@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 11:11:01 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/05/18 18:59:55 by manonlebiha      ###   ########.fr       */
+/*   Updated: 2022/05/20 16:59:06 by manonlebiha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,5 +34,6 @@ void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
