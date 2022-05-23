54e3936df15aca1b24a1f187d5fb97b4e093da78
Manon Le Bihan <manonlebihan1999@gmail.com>
2022-05-23 14:36:12 +0200

    functional ft_strchr

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,25 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: manonlebihan <manonlebihan@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 15:28:50 by macbookpro        #+#    #+#             */
-/*   Updated: 2022/05/16 15:35:42 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/05/23 12:56:04 by manonlebiha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-char	*ft_strchr(const char *str, int searchedChar)
+char	*ft_strchr(const char *s, int c)
 {
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
+	while (*s != c)
 	{
-		if (str[i] == searchedChar)
-			return ((char *)str + i);
-		i++;
+		if (!*s++)
+			return (NULL);
 	}
-	return (NULL);
+	return ((char *)s);
 }
