227a149b70cbca7406274005a72b5bbc194a4571
Manon Le Bihan <manonlebihan1999@gmail.com>
2022-05-23 23:16:27 +0200

    functional ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -3,44 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: manonlebihan <manonlebihan@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 11:00:15 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/05/18 14:04:06 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/05/23 23:14:59 by manonlebiha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void	*ft_memmove(void *dest, const void *src, size_t n)
+void	*ft_memmove(void *dst, const void *src, size_t n)
 {
-	char	*cdest;
-	char	*csrc;
-	size_t	i;
+	char		*d;
+	const char	*s;
 
-	cdest = (char *)dest;
-	csrc = (char *)src;
-	i = 0;
-	while (i < n)
+	d = (char *)dst;
+	s = (char *)src;
+	if (d < s)
 	{
-		cdest[i] = csrc[i];
-		i++;
+		while (n--)
+		*d++ = *s++;
 	}
-	return (cdest);
+	else
+	{
+		s = s + n - 1;
+		d = d + n - 1;
+		while (n--)
+			*d-- = *s--;
+	}
+	return (dst);
 }
-
-/* #include <string.h>
-int main(void)
-{
-    char dest[5] = "hello";
-    const char src[]  = "manonlbn";
-	char destt[5] = "hello";
-    const char srcc[]  = "manonlbn";
-    printf("Before ft_memmove >> dest = %s, src = %s\n", dest, src);
-    ft_memmove(dest, src, 8);
-    printf("After ft_memmove >> dest = %s, src = %s\n", dest, src);
-    printf("Before memmove >> dest = %s, src = %s\n", destt, srcc);
-    memmove(destt, srcc, 8);
-    printf("After memmove >> dest = %s, src = %s\n", destt, srcc);
-    return (0);
-} */
