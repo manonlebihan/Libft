7f28c9fcaf3d432fd63c3fb4dd10a0123dde983f
Manon Le Bihan <manonlebihan1999@gmail.com>
2022-05-23 19:46:28 +0200

    correct cast of arg c

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -3,35 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strrchr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
+/*   By: manonlebihan <manonlebihan@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 18:40:48 by macbookpro        #+#    #+#             */
-/*   Updated: 2022/05/16 15:34:52 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/05/23 19:43:48 by manonlebiha      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-size_t	ft_strlen(const char *str)
+size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
 	i = 0;
-	while (str[i] != '\0')
+	while (s[i] != '\0')
 		i++;
 	return (i);
 }
 
-char	*ft_strrchr(const char *str, int searchedChar)
+char	*ft_strrchr(const char *s, int c)
 {
 	int	i;
 
-	i = ft_strlen(str);
-	while (i != 0)
+	c = (unsigned char)c;
+	i = ft_strlen(s);
+	while (i != -1)
 	{
-		if (str[i] == searchedChar)
-			return ((char *)str + i);
+		if (s[i] == c)
+			return ((char *)s + i);
 		i--;
 	}
 	return (NULL);
 }
+
+/* #include <string.h>
+
+int	main(void)
+{
+	char s[] = "tripouille";
+	//char c = 't';
+	char *p, *p2;
+	p = ft_strrchr(s, 't' + 256);
+	p2 = strrchr(s, 't' + 256);
+	printf("ft_strrchr(s, c) = %s\n", p);
+	printf("strrchr(s, c) = %s\n", p2);
+	return (0);
+} */
