31a280dac46bb4649445a780e69c10ee8a7f19d9
manonlebihan <manonlebihan1999@gmail.com>
2022-06-03 15:53:00 +0200

    bullshit

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,65 +6,22 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 10:02:10 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/06/02 14:44:04 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/06/03 15:52:36 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "libft.h"
 
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
-
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-	size_t	src_size;
-
-	i = 0;
-	src_size = 0;
-	while (src[src_size] != '\0')
-		src_size++;
-	if (dstsize == 0)
-		return (src_size);
-	while (i < (dstsize - 1) && src[i] != '\0')
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (src_size);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char		*dst;
-	size_t		i;
-	int			dst_len;
-
-	dst_len = len - start;
-	i = start;
-	dst = (char *)malloc((dst_len + 1) * sizeof(char));
-	if (dst == NULL)
-		return (NULL);
-	while (i < len && (*(s + i) != '\0'))
-	{
-		*dst = *(s + i);
-		dst++;
-		i++;
-	}
-	*dst = '\0';
-	return (dst - dst_len);
-}
-
+// gonna need substr
+// Faire un malloc qql part
+// Vérification si allocation a marché
+	//if (s2 == NULL)
+	//  return (NULL);
+// valeur retournée (new string : s2)
+	//*s2 = '\0';
+// maybe add *set && im=n the while loops
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*s2;
@@ -73,21 +30,20 @@ char	*ft_strtrim(char const *s1, char const *set)
 	int		k;
 
 	i = 0;
-	j = 0;
-	k = ft_strlen(s1);
-	s2 = malloc(30);
-	ft_strlcpy(s2, s1, 30);
-	while (s1[0] != set[j])
-		j++;
+	j = ft_strlen(s1);
+	k = ft_strlen(set);
 
-	s2 = ft_substr(s2, 1, 18);
-// gonna need substr
-// Faire un malloc qql part
-// Vérification si allocation a marché
-    //if (s2 == NULL)
-     //   return (NULL);
-// valeur retournée (new string : s2)
-	//*s2 = '\0';
+	ft_strlcpy(s2, s1, (j + 1));
+	while (i != k && s2[0] != set[i])
+		i++;
+	if (s2[0] == set[i])
+		s2 = ft_substr(s2, 1, j);
+	i = 0;
+	j = ft_strlen(s2);
+	while (i != k && s2[j] != set[i])
+		i++;
+	if (s2[j] == set[i])
+		s2 = ft_substr(s2, 0, (j - 1));
 	return (s2);
 }
 
@@ -97,8 +53,8 @@ int	main(void)
 	char	*set;
 	char	*s3;
 
-	s1 = "Manon est gentille";
-	set = "eM";
+	s1 = "manon east la";
+	set = "manger";
 	s3 = ft_strtrim(s1, set);
 	printf("%s\n", s3);
 }
