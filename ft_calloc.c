3c4307c1b912d5ab99a9aa283d334640eae99dd8
manon.lebihan <manon.lebihan@eemi.com>
2022-06-06 22:25:28 +0200

    functional ft_calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -13,14 +13,19 @@
 #include <stdlib.h>
 #include "libft.h"
 
+#define MAX_SIZE_T (size_t)-1
+
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*p;
 
-	if ((nmemb * size) < nmemb || (nmemb * size) < size)
+	if (nmemb > 0 && (MAX_SIZE_T / nmemb) < size)
 		return (NULL);
 	if ((nmemb == 0) || (size == 0))
-		return (malloc(1));
+	{
+		nmemb = 1;
+		size = 1;
+	}
 	p = malloc(nmemb * size);
 	ft_bzero(p, nmemb * size);
 	return (p);
