07ec9ed6b3cb73a508f2a03bcf7d3ebf21c183a0
manonlebihan <manonlebihan1999@gmail.com>
2022-06-07 17:41:15 +0200

    functional ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/01 10:02:10 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/06/03 16:45:49 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/06/07 17:39:01 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,46 +24,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 	j = ft_strlen(s1);
 	while (ft_strchr(set, s1[i]))
 		i++;
-	while (ft_strchr(set, s1[j--]))
-	s2 = ft_substr(s1, i, j + 1);
+	while (ft_strchr(set, s1[j]) && j > i)
+		j--;
+	s2 = ft_substr(s1, i, (j - i + 1));
 	return (s2);
 }
-
-/* int	main(void)
-{
-	char	*s1;
-	char	*set;
-	char	*s3;
-
-	s1 = "coucou les zamis de 42 paris";
-	set = "siuoc";
-	s3 = ft_strtrim(s1, set);
-	printf("%s\n", s3);
-}
- */
-
-/* char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*s2;
-	int		i;
-	int		j;
-	int		k;
-
-	i = 0;
-	j = ft_strlen(s1);
-	k = ft_strlen(set);
-
-	ft_strlcpy(s2, s1, (j + 1));
-	while (i != k && s2[0] != set[i])
-		i++;
-	if (s2[0] == set[i])
-		s2 = ft_substr(s2, 1, j);
-	i = 0;
-	j = ft_strlen(s2);
-	while (i != k && s2[j] != set[i])
-		i++;
-	if (s2[j] == set[i])
-		s2 = ft_substr(s2, 0, (j - 1));
-	return (s2);
-}
- */
