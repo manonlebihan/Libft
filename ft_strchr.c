9d1ce480a32c56c0fe0e205de4aa88352de40924
manonlebihan <manonlebihan1999@gmail.com>
2022-06-07 16:56:27 +0200

    add warmachine tester

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 14:31:07 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/06/03 16:39:27 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/06/07 16:55:16 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 char	*ft_strchr(const char *s, int c)
 {
+	if (c > 256)
+		c %= 256;
 	while (*s != c)
 	{
 		if (!*s++)
@@ -21,33 +23,3 @@ char	*ft_strchr(const char *s, int c)
 	}
 	return ((char *)s);
 }
-
-/* #include <string.h>
-
-int	main(void)
-{
-	char s[] = "tripouille";
-	//char c = 't' + 256;
-   	char *p, *p2;
-	p = ft_strchr(s, 't' + 256);
-	p2 = strchr(s, 't' + 256);
-	printf("ft_strchr(s, 't' + 256) = %s\n", p);
-	printf("strchr(s, 't' + 256) = %s", p2);
-	return (0);
-}
- */
-/* char	*ft_strrchr(const char *s, int c)
-{
-	int	i;
-
-	c = (unsigned char)c;
-	i = ft_strlen(s);
-	while (i != -1)
-	{
-		if (s[i] == c)
-			return ((char *)s + i);
-		i--;
-	}
-	return (NULL);
-}
- */
