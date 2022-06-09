6c2e0cb538b70c5251fa09d700f63be402a4d36d
manonlebihan <manonlebihan1999@gmail.com>
2022-06-09 11:37:56 +0200

    TODO : ft_split

--- ft_split.c
+++ ft_split.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: manonlebihan <manonlebihan@student.42.f    +#+  +:+       +#+        */
+/*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/08 10:38:00 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/06/09 08:52:11 by manonlebiha      ###   ########.fr       */
+/*   Updated: 2022/06/09 11:37:00 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,25 @@
 #include <stdio.h>
 #include "libft.h"
 
-char	*ft_new_str(char *s, int s_start, int s_finish)
+int	ft_count_word(const char *s, char c)
 {
-	char	*new_str;
-	char	i;
+	int	i;
+	int	word_count;
 
-	new_str = malloc((s_finish - s_start + 1) * sizeof(char));
-	while (i < s_finish)
+	i = 0;
+	word_count = 1;
+	if (s[0] == '\0')
+		return (0);
+	while (s[i])
 	{
+		if (s[i] == c)
+			word_count++;
 		i++;
 	}
+	return (word_count);
+}
+
+char	**ft_split(char const *s, char c)
+{
 	
 }
-
-int	main(void)
-{
-	char	*s;
-	//char	c;
-	char	*res;
-
-	s = "manon est la";
-	//c = ' ';
-	res = ft_new_str(s, 0, 4);
-
-	printf("%d\n", res);
-}
-
-char = M A N O N   E S T   L  A
-inde = 0 1 2 3 4 5 6 7 8 9 10 11
-memo = 1 2 3 4 5 6 7 8 9 10 11 12
-
-M  A  N  O  N \0
-13 14 15 16 17 18
-
-ft_new_str("manon est la", 0, 4);
