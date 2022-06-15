77e0d3dc8ada05b9a4efb40200896e4151eeba27
manon.lebihan <manon.lebihan@eemi.com>
2022-06-15 16:55:55 +0200

    ft_lstadd_front.c

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,7 +6,7 @@
 /*   By: mle-biha <mle-biha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 14:38:40 by mle-biha          #+#    #+#             */
-/*   Updated: 2022/06/15 14:38:54 by mle-biha         ###   ########.fr       */
+/*   Updated: 2022/06/15 16:41:38 by mle-biha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	
+	if (lst)
+	{
+		if (*lst)
+			new->next = *lst;
+		*lst = new;
+	}
 }
